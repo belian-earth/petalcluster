96af6be0442d05a70b8c0c0cca01097874ce050f
Hugh Graham <hugh.a.graham@gmail.com>
2026-02-20 15:36:52 +0000

    add data documentation

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' rings
+#'
+#' A dataset of concentric rings with noise, used for testing clustering algorithms.
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'   \item{x}{X coordinate}
+#'   \item{y}{Y coordinate}
+#'   \item{cluster}{Cluster assignment}
+#' }
+#' @source Generated using a custom function.
+"rings"
